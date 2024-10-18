ae5e6303eb7abdc088f478ebe8bfb6c04fffd876
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2024-10-19 03:10:11 +0500

    commit

--- src/app/blog-post/page.tsx
+++ src/app/blog-post/page.tsx
@@ -9,12 +9,6 @@ export default async function BlogPosts() {
   const posts = await Posts();
 
   return (
-    <div className="max-w-5xl m-auto bg-white shadow-md">
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 items justify-items-center  gap-3 p-3">
-        {posts.map((post: any) => (
-          <Card postData={post} key={post.id} />
-        ))}
-      </div>
-    </div>
+    
   );
 }
