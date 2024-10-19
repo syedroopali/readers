56314b6bc4aed2937127e2f01817a9dc71e292ad
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2024-10-19 13:47:31 +0500

    commit

--- src/app/_components/SearchBar.tsx
+++ src/app/_components/SearchBar.tsx
@@ -6,24 +6,18 @@ import { useSearch } from "../_contexts/SearchContext";
 export default function SearchBar({ postData }: any) {
   const { search, setSearch, setSearchData, searchData }: any = useSearch();
 
-  const searchResult = postData.filter((obj: any) =>
+  const searchResult: string[] = postData.filter((obj: any) =>
     `${obj.title.toLowerCase()} ${obj.body.toLowerCase()}`
       .split(" ")
       .includes(search)
   );
 
-  const handleClick = function () {
-    if (searchResult.length) {
-      console.log(searchData);
-      setSearchData(searchResult);
-      console.log(searchData);
-    }
-    // setSearchData((search: string[]): string[] => [...search, searchResult]);
-  };
-
   const handleSubmit = function (e: any) {
-    setSearch("");
     e.preventDefault();
+    if (search.length) {
+      setSearchData(searchResult);
+      setSearch("");
+    }
   };
 
   return (
@@ -35,10 +29,7 @@ export default function SearchBar({ postData }: any) {
         placeholder="Type here..."
         className="border-black border-2 border-r-0 sm:py-1 px-2  rounded-l-md placeholder:text-md  text-md outline-none focus:shadow-md transition-all w-[80%] sm:max-w-md sm:flex-1"
       />
-      <button
-        className=" bg-yellow-500  sm:px-3 border-2 border-black  font-bold rounded-r-md"
-        onClick={handleClick}
-      >
+      <button className=" bg-yellow-500  sm:px-3 border-2 border-black  font-bold rounded-r-md">
         <p className="hidden text-xl sm:block">Search</p>
         <div className="sm:hidden px-2 py-1 text-2xl">
           <FiSearch />
