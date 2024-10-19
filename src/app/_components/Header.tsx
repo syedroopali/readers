39f3066488ccf7b779cc028e45325e4cf3b848b2
Syed-Roop-Ali-2729 <164709030+Syed-Roop-Ali-2729@users.noreply.github.com>
2024-10-19 12:16:53 +0500

    commit

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -4,7 +4,7 @@ import NavBarLinks from "./NavBarLinks";
 
 export default function Header() {
   return (
-    <header className="shadow-md p-3 flex items-center h-14 justify-between sm:h-16 md:h-[4.5rem] md:p-6 sm:p-4 w-full fixed top-0 bg-white">
+    <header className="shadow-md p-3 flex items-center h-14 justify-between sm:h-16 md:h-[4.5rem] md:p-6 sm:p-4 w-full fixed top-0 bg-white z-50">
       <Logo />
       <div className="flex-1 flex justify-end">
         <NavBarMobile />
